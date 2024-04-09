ea9be4750ce8a0b19290a67f2883bfca1bd5be62
szy her <szymonhernik123@gmail.com>
2024-04-09 12:43:31 +0200

    api version for stripe webhooks

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -6,7 +6,7 @@ export const stripe = new Stripe(
     // https://github.com/stripe/stripe-node#configuration
     // https://stripe.com/docs/api/versioning
     // @ts-ignore
-    apiVersion: null,
+    apiVersion: '2023-10-16',
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
