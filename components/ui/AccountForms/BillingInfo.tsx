5982541b0b32b66f1facc7e2eea8dced9d111c97
szy her <szymonhernik123@gmail.com>
2024-04-12 13:16:26 +0200

    Update to Next 12

--- components/ui/AccountForms/BillingInfo.tsx
+++ components/ui/AccountForms/BillingInfo.tsx
@@ -83,7 +83,7 @@ export default function BillingInfo({
         <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
           <p className="pb-4 sm:pb-0">Change your billing method.</p>
           <Dialog>
-            <DialogTrigger>
+            <DialogTrigger asChild>
               <Button
                 variant="slim"
                 loading={isSubmitting}
