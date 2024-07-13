9a30a1f0c80b5ddcc767d4ff0bc14838728aa04e
szy her <szymonhernik123@gmail.com>
2024-07-13 15:45:56 +0200

    remove f_auto and q_auto to HIT cache in bunny cdn

--- src/app/(pages)/(public)/post/[slug]/_components/PreviewVideo.tsx
+++ src/app/(pages)/(public)/post/[slug]/_components/PreviewVideo.tsx
@@ -18,14 +18,17 @@ export default function PreviewVideo(props: Props) {
   if (video && video.public_id && video.format) {
     return (
       <>
-        <video
-          className="h-full w-full"
-          playsInline
-          autoPlay
-          muted
-          loop
-          src={`https://cloud-lc.b-cdn.net/video/upload/w_960/q_auto/f_auto/${video.public_id}.${video.format}`}
-        ></video>
+        <video className="h-full w-full" playsInline autoPlay muted loop>
+          <source
+            src={`https://cloud-lc.b-cdn.net/video/upload/w_500/q_80/f_webm/${video.public_id}.webm`}
+            type="video/webm"
+          />
+          <source
+            src={`https://cloud-lc.b-cdn.net/video/upload/w_500/q_80/f_mp4/${video.public_id}.mp4`}
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
       </>
     )
   }
