be6ede296b2a601a99198908bf7da151037e19f5
szy her <szymonhernik123@gmail.com>
2024-08-17 07:39:10 +0200

    style highlight element

--- src/app/(pages)/(public)/browse/_components/main/DynamicDisplayBar.tsx
+++ src/app/(pages)/(public)/browse/_components/main/DynamicDisplayBar.tsx
@@ -9,22 +9,26 @@ export default function DynamicDisplayBar({ data }: Props) {
   const { title, coverImage, slug } = data?.highlight || {}
   if (!slug || !coverImage) return null
   return (
-    <Link href={`/browse/preview/${slug}`} className="w-1/2 h-full bg-blue-200">
-      <div className="p-4 uppercase flex gap-4 h-full items-center">
-        <div className="  aspect-square h-full max-h-32 ">
-          <ImageBoxArticle
-            image={coverImage}
-            width={200}
-            height={200}
-            size="200px"
-          />
+    <div className="w-full sm:w-3/4  h-full">
+      <Link href={`/browse/preview/${slug}`} className=" ">
+        <div className="px-4 py-6  flex gap-4 h-full items-center group ">
+          <div className="  aspect-square h-full max-h-32 group-hover:opacity-80">
+            <ImageBoxArticle
+              image={coverImage}
+              width={300}
+              height={300}
+              size="300px"
+            />
+          </div>
+          <div className="h-full pt-4 pl-2 ">
+            {' '}
+            <p className="text-secondary-foreground">Highlight</p>
+            <p className="font-semibold tracking-tight group-hover:opacity-80">
+              {title}
+            </p>
+          </div>
         </div>
-        <div className="h-full pt-2 pl-2">
-          {' '}
-          <p>Highlight</p>
-          <p className="font-semibold">{title}</p>
-        </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
