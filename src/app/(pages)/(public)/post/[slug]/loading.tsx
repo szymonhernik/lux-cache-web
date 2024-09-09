58f1ef7482ca6c78764a1b911c3a419f186b1a15
szy her <szymonhernik123@gmail.com>
2024-09-09 14:59:57 +0200

    Episode loading: Work on a loading state/skeleton

--- src/app/(pages)/(public)/post/[slug]/loading.tsx
+++ src/app/(pages)/(public)/post/[slug]/loading.tsx
@@ -1,9 +1,43 @@
+import { Button } from '@/components/shadcn/ui/button'
 import { LoadingSpinner } from '../../../../../components/Spinner'
+import { BookmarkIcon } from '@radix-ui/react-icons'
 
 export default function Loading() {
   return (
-    <div className="my-36 max-w-2xl mx-auto divide-y grid gap-4 px-4 text-center">
-      <LoadingSpinner className="animate-spin mx-auto" size={32} />
-    </div>
+    // <div className="my-36 max-w-2xl mx-auto divide-y grid gap-4 px-4 text-center">
+    //   <LoadingSpinner className="animate-spin mx-auto" size={32} />
+    // </div>
+    <>
+      <div className="flex items-center gap-4 p-4 sticky top-0 left-0 flex-row-reverse md:flex-row justify-between md:justify-start z-[10]">
+        <div className="flex gap-2">
+          <Button disabled={true}>PDF</Button>
+          <Button
+            variant={'outline'}
+            className="flex items-center gap-1 leading-2 "
+            disabled={true}
+          >
+            <BookmarkIcon width={16} height={16} />
+
+            <span className="hidden md:block">Bookmark</span>
+          </Button>
+        </div>
+
+        {/* <h1 className="text-shadow text-sm w-64 h-6 bg-secondary-foreground/10 rounded-sm animate-pulse font-semibold"></h1> */}
+      </div>
+      {/* <article className="max-w-4xl mx-auto">
+
+        <div className=" w-full mx-auto my-36 px-4  space-y-24  ">
+          <section className=" space-y-4 items-center w-full  ">
+            <p className="mx-auto text-sm tracking-tight text-center w-64 h-6 bg-secondary-foreground/10 rounded-sm animate-pulse"></p>
+            <div className="w-full space-y-2">
+              <h1 className="mx-auto text-center text-3xl tracking-tight text-pretty font-semibold uppercase w-96 h-12 bg-secondary-foreground/10 rounded-sm animate-pulse"></h1>
+
+              <h2 className="mx-auto text-center text-lg tracking-tight text-pretty font-semibold uppercase w-64 h-6 bg-secondary-foreground/10 rounded-sm animate-pulse"></h2>
+            </div>
+          </section>
+        </div>
+      </article> */}
+      <LoadingSpinner className="my-36 animate-spin mx-auto" size={32} />
+    </>
   )
 }
--- src/app/(pages)/(public)/post/[slug]/page.tsx
+++ src/app/(pages)/(public)/post/[slug]/page.tsx
@@ -9,13 +9,13 @@ import { Suspense } from 'react'
 import PostNavbar from './_components/PostNavbar'
 import { canAccessPost } from '@/utils/helpers/subscriptionUtils'
 import { Button } from '@/components/shadcn/ui/button'
+import { BookmarkIcon } from '@radix-ui/react-icons'
 
 type Props = {
   params: { slug: string }
 }
 export default async function ProjectSlugRoute({ params }: Props) {
   const initial = await loadPost(params.slug)
-  // freeze for 20 sec
 
   if (draftMode().isEnabled) {
     return <PostPreview params={params} initial={initial} />
@@ -25,7 +25,7 @@ export default async function ProjectSlugRoute({ params }: Props) {
     notFound()
   }
   const supabase = createClient()
-  const user = await getUser(supabase)
+  // const user = await getUser(supabase)
   const userTierObject = await getUserTier(supabase)
   const userTier = userTierObject?.userTier
   const canAccess = canAccessPost(userTier, initial.data.minimumTier)
@@ -37,7 +37,20 @@ export default async function ProjectSlugRoute({ params }: Props) {
     <>
       <div className="flex items-center gap-4 p-4 sticky top-0 left-0 flex-row-reverse md:flex-row justify-between md:justify-start z-[10]">
         <Suspense
-          fallback={<Button variant={'outline'} isLoading={true}></Button>}
+          fallback={
+            <div className="flex gap-2">
+              <Button disabled={true}>PDF</Button>
+              <Button
+                variant={'outline'}
+                className="flex items-center gap-1 leading-2 "
+                disabled={true}
+              >
+                <BookmarkIcon width={16} height={16} />
+
+                <span className="hidden md:block">Bookmark</span>
+              </Button>
+            </div>
+          }
         >
           {initial.data._id && (
             <PostNavbar title={initial.data.title} post_id={initial.data._id} />
