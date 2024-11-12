a8439a429115ad2886ac2dad6bac88015b5ef325
szy her <szymonhernik123@gmail.com>
2024-11-12 18:01:48 +0100

    feat(newsletter): add Mailjet integration and newsletter signup form (testing!)

---
+++ src/app/(pages)/(public)/newsletter/page.tsx
@@ -0,0 +1,9 @@
+import NewsletterForm from './_components/NewsletterForm/page'
+
+export default function Page() {
+  return (
+    <div className="flex w-1/2 mx-auto justify-center items-center h-screen">
+      <NewsletterForm />
+    </div>
+  )
+}
