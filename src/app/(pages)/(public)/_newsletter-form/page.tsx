5e143bb5ca320ec53a26ed4a91bdd2a1fc97cd24
szy her <szymonhernik123@gmail.com>
2024-11-13 13:32:59 +0100

    remove old newsletter route

--- src/app/(pages)/(public)/_newsletter-form/page.tsx
+++
@@ -1,9 +0,0 @@
-// import NewsletterForm from './_components/NewsletterForm/page'
-
-export default function Page() {
-  return (
-    <div className="flex w-1/2 mx-auto justify-center items-center h-screen">
-      {/* <NewsletterForm /> */}
-    </div>
-  )
-}
