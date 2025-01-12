953c43d5ccf36faf80d722162caa239f4f069ce2
szy her <szymonhernik123@gmail.com>
2025-01-12 17:50:50 +0100

    improve the check for the user role to display the batch in account page

--- src/app/(pages)/(account)/layout.tsx
+++ src/app/(pages)/(account)/layout.tsx
@@ -4,9 +4,13 @@ import RoleBadge from './account/_components/RoleBadge'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const userRole = headers().get('x-user-role')
+  console.log('userRole', userRole)
+  // userRole is a string so check if it is not null by converting the string to a boolean
+  const userRoleIsNotNull = userRole !== 'null' && userRole !== null
+
   return (
     <div className={'lg:flex xl:max-w-screen-lg xl:mx-auto'}>
-      {userRole && <RoleBadge userRole={userRole} />}
+      {userRoleIsNotNull && <RoleBadge userRole={userRole} />}
       <NavbarAccount />
       <section className="w-full lg:max-w-screen-sm mx-auto px-4 mb-32 pt-16 lg:mt-32 ">
         {children}
