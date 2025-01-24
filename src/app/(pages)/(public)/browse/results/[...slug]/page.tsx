6c4c1416698f99654a74ab1c68f878f0ba61da67
szy her <szymonhernik123@gmail.com>
2025-01-24 13:14:53 +0100

    also add suspense spinner for browse/results

--- src/app/(pages)/(public)/browse/results/[...slug]/page.tsx
+++ src/app/(pages)/(public)/browse/results/[...slug]/page.tsx
@@ -1,9 +1,25 @@
+import { Suspense } from 'react'
 import { fetchDataByCategory } from '@/utils/fetch-helpers/server'
 import ResultsPage from './_components/ResultsPage'
+import { LoadingSpinner } from '@/components/Spinner'
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default function Page({ params }: { params: { slug: string } }) {
+  return (
+    <Suspense fallback={<SpinnerContainer />}>
+      <ResultsPageContent params={params} />
+    </Suspense>
+  )
+}
+function SpinnerContainer() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <LoadingSpinner />
+    </div>
+  )
+}
+
+async function ResultsPageContent({ params }: { params: { slug: string } }) {
   const [category, identifier] = params.slug
-
   const initial = await fetchDataByCategory(category, identifier)
 
   if (!initial || initial?.data?.results === null) {
