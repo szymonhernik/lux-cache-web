121117bab3953838368ba14146b5faf6f4f8efa3
szy her <szymonhernik123@gmail.com>
2025-02-03 18:10:47 +0100

    fix the newsletter script on page navigation

--- src/app/(newsletter)/newsletter/_components/MailJet.tsx
+++ src/app/(newsletter)/newsletter/_components/MailJet.tsx
@@ -2,35 +2,57 @@
 import Logo from '@/components/icons/Logo'
 import Link from 'next/link'
 import Script from 'next/script'
+import { useLayoutEffect } from 'react'
+import { useState } from 'react'
 
 export default function MailJet() {
+  const [mounted, setMounted] = useState(false)
+
+  useLayoutEffect(() => {
+    setMounted(true)
+
+    return () => {
+      // Cleanup script when component unmounts
+      const script = document.querySelector(
+        'script[src="https://app.mailjet.com/pas-nc-embedded-v1.js"]'
+      )
+      if (script) {
+        script.remove()
+      }
+    }
+  }, [])
+
   return (
-    <div className="h-auto w-auto mx-auto flex flex-col mt-24 gap-12">
-      <div className="mx-auto">
+    <div className="h-auto w-auto mx-auto flex flex-col mt-24 gap-12 px-4">
+      <div className="mx-auto ">
         <Link href="/">
           <Logo width="120" height="80" alt="Lux Cache" />
         </Link>
       </div>
-      <Script
-        src="https://app.mailjet.com/pas-nc-embedded-v1.js"
-        strategy="afterInteractive"
-        onLoad={() => {
-          // @ts-ignore (iFrameResize is added by the script)
-          if (window.iFrameResize) {
-            // @ts-ignore
-            window.iFrameResize(
-              { checkOrigin: false },
-              '[data-w-type="embedded"]'
-            )
-          }
-        }}
-      />
-      <iframe
-        data-w-type="embedded"
-        src="https://s6hio.mjt.lu/wgt/s6hio/xgvz/form?c=bbda019e"
-        width="100%"
-        style={{ height: '0' }}
-      ></iframe>
+      {mounted && (
+        <>
+          <Script
+            src="https://app.mailjet.com/pas-nc-embedded-v1.js"
+            strategy="afterInteractive"
+            onLoad={() => {
+              // @ts-ignore (iFrameResize is added by the script)
+              if (window.iFrameResize) {
+                // @ts-ignore
+                window.iFrameResize(
+                  { checkOrigin: false },
+                  '[data-w-type="embedded"]'
+                )
+              }
+            }}
+          />
+          <iframe
+            data-w-type="embedded"
+            src="https://s6hio.mjt.lu/wgt/s6hio/xgvz/form?c=bbda019e"
+            width="100%"
+            style={{ height: '0' }}
+          ></iframe>
+        </>
+      )}
     </div>
   )
 }
--- src/app/(newsletter)/newsletter/page.tsx
+++ src/app/(newsletter)/newsletter/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react'
 import MailJet from './_components/MailJet'
+import { SpinnerContainer } from '@/components/shared/SpinnerContainer'
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<SpinnerContainer />}>
       <MailJet />
     </Suspense>
   )
