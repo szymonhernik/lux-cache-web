5a6be0fa81bf9af4fa4f513cb1bdb238c64ba08f
szy her <szymonhernik123@gmail.com>
2025-09-20 16:59:26 +0200

    update product with the correct currency

--- src/utils/stripe/products.ts
+++ src/utils/stripe/products.ts
@@ -2,7 +2,7 @@ export interface SubscriptionTiers {
   [key: string]: number
 }
 const subscriptionTiers = {
-  prod_T5ZlLfG76XRzpz: 0, // Free Trial (€0)
+  prod_T5dVQFsPzDsPUs: 0, // Free Trial (€0)
   prod_Q6V3i1qIusr0Tl: 1, // Basic Plan (€5)
   prod_QWLrcmPbR87pZd: 2 // Premium Plan (€10)
 }
