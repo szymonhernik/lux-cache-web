4233459416ca40f2c2bc82945f1d90e8b646631f
szy her <szymonhernik123@gmail.com>
2025-09-20 15:10:01 +0200

    update products id with the new free trial price

--- src/utils/stripe/products.ts
+++ src/utils/stripe/products.ts
@@ -2,7 +2,8 @@ export interface SubscriptionTiers {
   [key: string]: number
 }
 const subscriptionTiers = {
-  prod_Q6V3he057zQPIM: 1, // Supporter
+  // prod_Q6V3he057zQPIM: 1, // Supporter
+  prod_T5ZlLfG76XRzpz: 1, // Free Trial
   prod_Q6V3i1qIusr0Tl: 2, // Subscriber
   prod_QWLrcmPbR87pZd: 3 // Premium Subscriber
 }
